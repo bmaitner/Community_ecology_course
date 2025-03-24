e8557336466f7a8e1884a4bc164f7cb79fcd4b46
Brian <bmaitner@gmail.com>
2025-03-24 15:14:56 -0400

    lecture 17

--- R_code_for_lectures/community_assembly.R
+++ R_code_for_lectures/community_assembly.R
@@ -1,1 +1,15 @@
 # Community assembly
+
+hist(rexp(n = 100000,rate = .1))
+
+library(ggplot2)
+
+ggplot(data = data.frame(x=rexp(n = 100000,rate = .1)),
+       mapping = aes(x=x))+
+  geom_density(fill="hotpink")+
+  scale_x_continuous(expand=c(0,0))+
+  scale_y_continuous(expand=c(0,0))+
+  theme_bw()+
+  xlab("Dispersal (km)")
+
+max(rexp(n = 100000,rate = .1))
